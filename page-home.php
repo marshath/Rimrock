6453a34de87b586b790a43475d84ea3876dd8926
Theodore Marshall <ted@tedm.net>
2019-03-12 09:07:05 -0700

    add videos and photos sections to home page

--- page-home.php
+++ page-home.php
@@ -30,11 +30,106 @@
 
 						</article>
 						
-						
-
-						<article id="post-<?php the_ID(); ?>" <?php post_class( 'home-video' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+						<article id="post-<?php the_ID(); ?>" <?php post_class( 'main-wrap home-videos' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+								
+							<header>
+								<h3>Videos</h3>
+								<div class="home-btns"><a href="/videos/" class="btn">View all videos</a></div>
+							</header>
 
 							<section class="entry-content" itemprop="articleBody">
+								
+								<?php // new query on videos page
+			
+									$vids = new WP_Query('page_id=14');
+																		
+									if ( $vids->have_posts() ) : $vids->the_post();
+									
+										// check if the repeater field has rows of data
+										if( have_rows('video') ):
+									
+										echo '<div class="video">';
+										
+										 	// loop through the rows of data
+										 	while ( have_rows('video') ) : the_row();
+										 		// counter to break loop
+										 		$i++;
+										 		if( $i > 3 ): 
+													break;
+												endif;
+										
+												// display videos
+										
+												echo '<div class="video-wrap">
+													<a href="video-link">
+												
+														<div class="video-cover">
+															<figure><img src="', the_sub_field('video_image'), '" alt=""></figure>
+															</figure>
+														</div>
+														
+														<div class="video-info">
+														
+															<h3 class="video-title">', the_sub_field('video_title'), '</h3>
+															
+															<div class="video-details">
+																<span class="video-year">', the_sub_field('video_year'), '</span> • <span class="video-duration">', the_sub_field('video_duration'), ' min</span>
+															</div>
+														
+														</div>
+													</a>
+												</div>';
+										
+										endwhile;
+									
+										echo '</div>';
+										
+										endif;
+									
+									endif;
+									wp_reset_postdata()
+								?>
+							</section>
+
+						</article>
+						
+						<article id="post-<?php the_ID(); ?>" <?php post_class( 'main-wrap home-photos' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+								
+							<header>
+								<h3>Photos</h3>
+								<div class="home-btns"><a href="/photos/" class="btn">View all photos</a></div>
+							</header>
+
+							<section class="entry-content" itemprop="articleBody">
+								
+								<?php // new query on photos page
+			
+									$pics = new WP_Query('page_id=16');
+																		
+									if ( $pics->have_posts() ) : $pics->the_post();
+									
+										echo '<div class="photo-wrap">
+											<ul class="gallery">';
+											
+												$images = get_field('photo_gallery');
+												$rand1 = array_rand($images);
+												$rand2 = array_rand($images);
+												$rand3 = array_rand($images);
+											
+											    if( $images ):
+													echo '<li><a href="'. $images[$rand1]['url'] .'">'. wp_get_attachment_image( $images[$rand1]['ID'], $size['medium'] ) .'</a></li>'; 
+													echo '<li><a href="'. $images[$rand2]['url'] .'">'. wp_get_attachment_image( $images[$rand2]['ID'], $size['medium'] ) .'</a></li>'; 
+													echo '<li><a href="'. $images[$rand3]['url'] .'">'. wp_get_attachment_image( $images[$rand3]['ID'], $size['medium'] ) .'</a></li>'; 
+												endif;
+											
+											echo '</ul>
+										</div>';
+									
+									endif;
+										
+									wp_reset_postdata()
+								?>
+								
 								<?php // the content
 									the_content();
 								?>
